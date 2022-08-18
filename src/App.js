2334e097824cb9c5ce8ab3859027964ef4bf7f0d
Andy Menutti <andymenutti@gmail.com>
2022-08-18 17:00:43 -0300

    add calculator.js and fix App.css

--- src/App.js
+++ src/App.js
@@ -1,29 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import Calculator from './components/calculator';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => (
+  <>
+    <div>
+      <div className="container">
+        <form>
+          <input type="text" value={0} />
+        </form>
+        <div className="keypad">
+
+          <button type="button" id="clr">Clear</button>
+          <button type="button" id="lite">C</button>
+          <button type="button" id="lite">&divide;</button>
+          <button type="button">7</button>
+          <button type="button">8</button>
+          <button type="button">9</button>
+          <button type="button" id="lite">&times;</button>
+          <button type="button">4</button>
+          <button type="button">5</button>
+          <button type="button">6</button>
+          <button type="button" id="lite">-</button>
+          <button type="button">1</button>
+          <button type="button">2</button>
+          <button type="button">3</button>
+          <button type="button" id="lite">+</button>
+          <button type="button">0</button>
+          <button type="button" id="lite">.</button>
+          <button type="button" id="equal">=</button>
+        </div>
+      </div>
     </div>
-  );
-}
-
+  </>
+);
+Calculator.defaultValue = {};
 export default App;
