6e2c64c4f8dd2e5b256c00489eb36b16ef6249b0
Andy Menutti <andymenutti@gmail.com>
2022-08-18 18:25:50 -0300

    fix App.js & Calculator.js

--- src/App.js
+++ src/App.js
@@ -1,38 +1,12 @@
 import './App.css';
 import React from 'react';
-import Calculator from './components/calculator';
+import Calculator from './components/Calculator';
 
 const App = () => (
   <>
-    <div>
-      <div className="container">
-        <form>
-          <input type="text" value={0} />
-        </form>
-        <div className="keypad">
+    <Calculator />
 
-          <button type="button" id="clr">Clear</button>
-          <button type="button" id="lite">C</button>
-          <button type="button" id="lite">&divide;</button>
-          <button type="button">7</button>
-          <button type="button">8</button>
-          <button type="button">9</button>
-          <button type="button" id="lite">&times;</button>
-          <button type="button">4</button>
-          <button type="button">5</button>
-          <button type="button">6</button>
-          <button type="button" id="lite">-</button>
-          <button type="button">1</button>
-          <button type="button">2</button>
-          <button type="button">3</button>
-          <button type="button" id="lite">+</button>
-          <button type="button">0</button>
-          <button type="button" id="lite">.</button>
-          <button type="button" id="equal">=</button>
-        </div>
-      </div>
-    </div>
   </>
 );
-Calculator.defaultValue = {};
+
 export default App;
