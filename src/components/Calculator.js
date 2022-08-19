cf740bf4c442f486fef31ae49e6224e8d18ac3bb
Andy Menutti <andymenutti@gmail.com>
2022-08-19 11:54:15 -0300

    fix Calculator.js: assign logic

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,35 +1,59 @@
-const Calculator = () => {
-  (
-    <>
-      <div>
-        <div className="container">
-          <form>
-            <input type="text" value={0} />
-          </form>
-          <div className="keypad">
-            <button type="button" id="clr">Clear</button>
-            <button type="button" id="lite">C</button>
-            <button type="button" id="lite">&divide;</button>
-            <button type="button">7</button>
-            <button type="button">8</button>
-            <button type="button">9</button>
-            <button type="button" id="lite">&times;</button>
-            <button type="button">4</button>
-            <button type="button">5</button>
-            <button type="button">6</button>
-            <button type="button" id="lite">-</button>
-            <button type="button">1</button>
-            <button type="button">2</button>
-            <button type="button">3</button>
-            <button type="button" id="lite">+</button>
-            <button type="button">0</button>
-            <button type="button" id="lite">.</button>
-            <button type="button" id="equal">=</button>
+import React from 'react';
+import calculate from '../logic/calculate';
+
+class Calculator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      obj: {
+        total: null,
+        next: null,
+        operation: null,
+      },
+    };
+    this.result = this.result.bind(this);
+  }
+
+  result(e) {
+    const { obj } = this.state;
+    const newObject = calculate(obj, e.target.textContent);
+    this.setState({ obj: newObject });
+  }
+
+  render() {
+    const { obj } = this.state;
+    return (
+      <>
+        <div>
+          <div className="container">
+            <form>
+              <input type="text" value={obj.next || obj.total || 0} />
+            </form>
+            <div className="keypad">
+              <button type="button" className="button" onClick={this.result} id="clr">Clear</button>
+              <button type="button" className="button" onClick={this.result} id="lite">C</button>
+              <button type="button" className="button" onClick={this.result} id="lite">&divide;</button>
+              <button type="button" className="button" onClick={this.result}>7</button>
+              <button type="button" className="button" onClick={this.result}>8</button>
+              <button type="button" className="button" onClick={this.result}>9</button>
+              <button type="button" className="button operation_button" onClick={this.result} id="lite">x</button>
+              <button type="button" className="button" onClick={this.result}>4</button>
+              <button type="button" className="button" onClick={this.result}>5</button>
+              <button type="button" className="button" onClick={this.result}>6</button>
+              <button type="button" className="button operation_button" onClick={this.result} id="lite">-</button>
+              <button type="button" className="button" onClick={this.result}>1</button>
+              <button type="button" className="button" onClick={this.result}>2</button>
+              <button type="button" className="button" onClick={this.result}>3</button>
+              <button type="button" className="button operation_button" onClick={this.result} id="lite">+</button>
+              <button type="button" className="button" onClick={this.result}>0</button>
+              <button type="button" className="button" onClick={this.result} id="lite">.</button>
+              <button type="button" className="button operation_button" onClick={this.result} id="equal">=</button>
+            </div>
           </div>
         </div>
-      </div>
-    </>
-  );
-};
+      </>
+    );
+  }
+}
 
 export default Calculator;
