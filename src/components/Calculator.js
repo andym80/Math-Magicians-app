78586e6287bffe7f354a4e60c661072b4d24c096
Andy Menutti <andymenutti@gmail.com>
2022-08-19 14:49:26 -0300

    fix calculator.js :hooks

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,59 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import calculate from '../logic/calculate';
 
-class Calculator extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      obj: {
-        total: null,
-        next: null,
-        operation: null,
-      },
-    };
-    this.result = this.result.bind(this);
-  }
+const Calculator = () => {
+  const [obj, addNewObject] = useState({ total: null, next: null, operation: null });
 
-  result(e) {
-    const { obj } = this.state;
-    const newObject = calculate(obj, e.target.textContent);
-    this.setState({ obj: newObject });
-  }
-
-  render() {
-    const { obj } = this.state;
-    return (
-      <>
-        <div>
-          <div className="container">
-            <form>
-              <input type="text" value={obj.next || obj.total || 0} />
-            </form>
-            <div className="keypad">
-              <button type="button" className="button" onClick={this.result} id="clr">Clear</button>
-              <button type="button" className="button" onClick={this.result} id="lite">C</button>
-              <button type="button" className="button" onClick={this.result} id="lite">&divide;</button>
-              <button type="button" className="button" onClick={this.result}>7</button>
-              <button type="button" className="button" onClick={this.result}>8</button>
-              <button type="button" className="button" onClick={this.result}>9</button>
-              <button type="button" className="button operation_button" onClick={this.result} id="lite">x</button>
-              <button type="button" className="button" onClick={this.result}>4</button>
-              <button type="button" className="button" onClick={this.result}>5</button>
-              <button type="button" className="button" onClick={this.result}>6</button>
-              <button type="button" className="button operation_button" onClick={this.result} id="lite">-</button>
-              <button type="button" className="button" onClick={this.result}>1</button>
-              <button type="button" className="button" onClick={this.result}>2</button>
-              <button type="button" className="button" onClick={this.result}>3</button>
-              <button type="button" className="button operation_button" onClick={this.result} id="lite">+</button>
-              <button type="button" className="button" onClick={this.result}>0</button>
-              <button type="button" className="button" onClick={this.result} id="lite">.</button>
-              <button type="button" className="button operation_button" onClick={this.result} id="equal">=</button>
-            </div>
+  const result = (event) => {
+    const newObject = calculate(obj, event.target.textContent);
+    addNewObject(newObject);
+  };
+  return (
+    <>
+      <div>
+        <div className="container">
+          <form>
+            <input type="text" value={obj.next || obj.total || 0} />
+          </form>
+          <div className="keypad">
+            <button type="button" className="button" onClick={result} id="clr">Clear</button>
+            <button type="button" className="button" onClick={result} id="lite">C</button>
+            <button type="button" className="button" onClick={result} id="lite">&divide;</button>
+            <button type="button" className="button" onClick={result}>7</button>
+            <button type="button" className="button" onClick={result}>8</button>
+            <button type="button" className="button" onClick={result}>9</button>
+            <button type="button" className="button operation_button" onClick={result} id="lite">x</button>
+            <button type="button" className="button" onClick={result}>4</button>
+            <button type="button" className="button" onClick={result}>5</button>
+            <button type="button" className="button" onClick={result}>6</button>
+            <button type="button" className="button operation_button" onClick={result} id="lite">-</button>
+            <button type="button" className="button" onClick={result}>1</button>
+            <button type="button" className="button" onClick={result}>2</button>
+            <button type="button" className="button" onClick={result}>3</button>
+            <button type="button" className="button operation_button" onClick={result} id="lite">+</button>
+            <button type="button" className="button" onClick={result}>0</button>
+            <button type="button" className="button" onClick={result} id="lite">.</button>
+            <button type="button" className="button operation_button" onClick={result} id="equal">=</button>
           </div>
         </div>
-      </>
-    );
-  }
-}
+      </div>
+    </>
+  );
+};
 
 export default Calculator;
